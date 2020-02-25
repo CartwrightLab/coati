e2611fa7d03dc70f021ba2c883da63bac44e7921
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2020-02-25 15:56:02 -0700

    Change email

--- modules/FindFst.cmake
+++ modules/FindFst.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Juan J. Garcia Mesa <jgarc111@asu.edu>
+# Copyright (c) 2020 Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
