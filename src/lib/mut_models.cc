56c6d20e14c4bf4d4ea52c64d5d0e5bae49c343e
Juan J. Garcia Mesa <juanjosegarciamesa@gmail.com>
2020-07-25 20:04:11 -0700

    Make nucs and codons unordered_maps global variables for speed

--- src/lib/mut_models.cc
+++ src/lib/mut_models.cc
@@ -23,6 +23,21 @@
 #include <doctest/doctest.h>
 #include <coati/mut_models.hpp>
 
+
+unordered_map<char, int> nucs = {{'A',0},{'C',1},{'G',2},{'T',3}};
+unordered_map<string, int> codons = {{"AAA",0},{"AAC",1},\
+{"AAG",2},{"AAT",3},{"ACA",4},{"ACC",5},{"ACG",6},{"ACT",7},\
+{"AGA",8},{"AGC",9},{"AGG",10},{"AGT",11},{"ATA",12},{"ATC",13},\
+{"ATG",14},{"ATT",15},{"CAA",16},{"CAC",17},{"CAG",18},{"CAT",19},\
+{"CCA",20},{"CCC",21},{"CCG",22},{"CCT",23},{"CGA",24},{"CGC",25},\
+{"CGG",26},{"CGT",27},{"CTA",28},{"CTC",29},{"CTG",30},{"CTT",31},\
+{"GAA",32},{"GAC",33},{"GAG",34},{"GAT",35},{"GCA",36},{"GCC",37},\
+{"GCG",38},{"GCT",39},{"GGA",40},{"GGC",41},{"GGG",42},{"GGT",43},\
+{"GTA",44},{"GTC",45},{"GTG",46},{"GTT",47},{"TAA",48},{"TAC",49},\
+{"TAG",50},{"TAT",51},{"TCA",52},{"TCC",53},{"TCG",54},{"TCT",55},\
+{"TGA",56},{"TGC",57},{"TGG",58},{"TGT",59},{"TTA",60},{"TTC",61},\
+{"TTG",62},{"TTT",63}};
+
 /* Muse & Gaut Model (1994) P matrix */
 void mg94_p(Matrix64f& P) {
 	// Yang (1994) estimating the pattern of nucleotide substitution
@@ -511,7 +526,6 @@ void ecm_marginal(VectorFst<StdArc>& mut_fst) {
 
 /* Dynamic Programming implementation of Marginal MG94 model*/
 vector<string> dp_mg94_marginal(vector<string> sequences, float& w) {
-	unordered_map<char, int> nucs = {{'A',0},{'C',1},{'G',2},{'T',3}};
 
 	// P matrix for marginal Muse and Gaut codon model
 	Eigen::Tensor<double, 3> p(64,3,4);
@@ -651,19 +665,6 @@ vector<string> dp_mg94_marginal(vector<string> sequences, float& w) {
 
 /* Return value from marginal MG94 model p matrix for a given transition */
 double transition(string codon, int position, char nucleotide, Eigen::Tensor<double, 3>& p) {
-	unordered_map<char, int> nucs = {{'A',0},{'C',1},{'G',2},{'T',3}};
-	unordered_map<string, int> codons = {{"AAA",0},{"AAC",1},\
-	{"AAG",2},{"AAT",3},{"ACA",4},{"ACC",5},{"ACG",6},{"ACT",7},\
-	{"AGA",8},{"AGC",9},{"AGG",10},{"AGT",11},{"ATA",12},{"ATC",13},\
-	{"ATG",14},{"ATT",15},{"CAA",16},{"CAC",17},{"CAG",18},{"CAT",19},\
-	{"CCA",20},{"CCC",21},{"CCG",22},{"CCT",23},{"CGA",24},{"CGC",25},\
-	{"CGG",26},{"CGT",27},{"CTA",28},{"CTC",29},{"CTG",30},{"CTT",31},\
-	{"GAA",32},{"GAC",33},{"GAG",34},{"GAT",35},{"GCA",36},{"GCC",37},\
-	{"GCG",38},{"GCT",39},{"GGA",40},{"GGC",41},{"GGG",42},{"GGT",43},\
-	{"GTA",44},{"GTC",45},{"GTG",46},{"GTT",47},{"TAA",48},{"TAC",49},\
-	{"TAG",50},{"TAT",51},{"TCA",52},{"TCC",53},{"TCG",54},{"TCT",55},\
-	{"TGA",56},{"TGC",57},{"TGG",58},{"TGT",59},{"TTA",60},{"TTC",61},\
-	{"TTG",62},{"TTT",63}};
 
 	position = position == 0 ? 2 : --position;
 
@@ -727,7 +728,6 @@ float alignment_score(vector<string> alignment) {
 	// P matrix for marginal Muse and Gaut codon model
 	Eigen::Tensor<double, 3> p(64,3,4);
 	mg94_marginal_p(p);
-	unordered_map<char, int> nucs = {{'A',0},{'C',1},{'G',2},{'T',3}};
 
 	string seq1 = alignment[0];
 	boost::erase_all(seq1, "-");
